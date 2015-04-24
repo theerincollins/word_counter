229b1c3c635517ab173c809162f5f761fa0eff52
theerincollins <erincollinsthethird@gmail.com>
2015-04-24 09:41:51 -0700

    Passes first rspec test

---
+++ lib/word_times.rb
@@ -0,0 +1,10 @@
+class String
+  def word_times(test_string)
+    frequency = 0
+    if self == test_string
+      frequency = frequency + 1
+    end
+
+
+  end
+end
--- spec/word_times_spec.rb
+++ spec/word_times_spec.rb
@@ -4,8 +4,8 @@ require('word_times')
 
 describe('String#word_times') do
 
-  it('returns a Fixnum value equal to the frequency of a word appears in a given string including only that word') do
-    expect("dog".word_times("dog")).to(eq.(1))
+  it('returns a Fixnum value equal to the frequency of a word in a given string including only that word') do
+    expect("dog".word_times("dog")).to(eq(1))
   end
 
 end
