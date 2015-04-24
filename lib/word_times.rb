c50cd8526c1305740ef3983f041ccd80213c40e8
theerincollins <erincollinsthethird@gmail.com>
2015-04-24 10:29:09 -0700

    passes the rspec test to remove punctuation from the input text string

--- lib/word_times.rb
+++ lib/word_times.rb
@@ -1,7 +1,9 @@
 class String
   def word_times(test_word)
     frequency = 0
-    input_words = self.split(" ")
+#gsub will remove any non-letters from the either text string then split will
+#turn the string into an array of words"
+    input_words = self.gsub(/[^a-z\s]/, "").split(" ")
     input_words.each do |word|
       if word == test_word
         frequency = frequency + 1
--- spec/word_times_spec.rb
+++ spec/word_times_spec.rb
@@ -4,12 +4,19 @@ require('word_times')
 
 describe('String#word_times') do
 
-  it('returns a Fixnum value equal to the frequency of a word in a given string including only that word') do
+  it('returns a Fixnum value equal to the frequency of a word in a given string
+  including only that word') do
     expect("dog".word_times("dog")).to(eq(1))
   end
 
-  it('returns a Fixnum value equal to the frequeny of a word in a given string of multiple words only') do
+  it('returns a Fixnum value equal to the frequeny of a word in a given string
+  of multiple words only') do
     expect("this dog that dog red dog small dog".word_times("dog")).to(eq(4))
+  end
+
+  it('returns a Fixnum value equal to the frequency of a word in a given stirng
+  of multiple words and punctuation') do
+    expect("this dog, that dog".word_times("dog")).to(eq(2))
   end
 
 end
