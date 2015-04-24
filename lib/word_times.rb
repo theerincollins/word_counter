6dc76bce885f5c471bba21bdf44b944a62673136
theerincollins <erincollinsthethird@gmail.com>
2015-04-24 11:07:45 -0700

    fails first integration test

--- lib/word_times.rb
+++ lib/word_times.rb
@@ -1,9 +1,10 @@
 class String
   def word_times(test_word)
     frequency = 0
-#gsub will remove any non-letters from the either text string then split will
+#gsub will remove any non-letters from either text string then split will
 #turn the string into an array of words"
     input_words = self.gsub(/[^a-z\s]/, "").split(" ")
+    test_word.gsub!(/[^a-z\s]/, "")
     input_words.each do |word|
       if word == test_word
         frequency = frequency + 1
---
+++ spec/word_times_integration_spec.rb
@@ -0,0 +1,17 @@
+require('capybara/rspec')
+require('./app')
+
+Capybara.app = Sinatra::Application
+
+describe('word count path', {:type => :feature}) do
+
+  it ('processes a one word sting and a multiple words string from user and returns
+  the frequency of the one word string within the multiple word string') do
+    visit('/')
+    fill_in('test_word', :with => 'shrimp')
+    fill_in('input_string', :with => 'grilled shrimp, fried shrimp, siracha shrimp')
+    click_button('Count!')
+    expect(page).to have_content(3)
+  end
+
+end
