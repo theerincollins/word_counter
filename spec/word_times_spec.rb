00b37d077a52fe0dbd41328c3ecd94e82c4634c7
theerincollins <erincollinsthethird@gmail.com>
2015-04-24 09:27:43 -0700

    fails initial rspec test

---
+++ spec/word_times_spec.rb
@@ -0,0 +1,11 @@
+require('rspec')
+require('pry')
+require('word_times')
+
+describe('String#word_times') do
+
+  it('returns a Fixnum value equal to the frequency of a word appears in a given string including only that word') do
+    expect("dog".word_times("dog")).to(eq.(1))
+  end
+
+end
